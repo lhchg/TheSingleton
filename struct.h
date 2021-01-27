319cc7da5712f4ad2942718772e2e8621ed2d848
Haocheng Li <lhao_cheng@163.com>
2021-01-27 23:12:42 +0800

    add struct

---
+++ struct.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Singleton.hpp"
+
+namespace StructInheritance {
+	struct Argument : public Singleton<Argument>
+	{
+		union {
+			struct {
+				int width;
+				int height;
+			};
+		};
+
+	};
+}
